b862fd561b3e960e612821042bfd5a42644ccf43
Eduardo Zawitoski <eduardozawitoski@gmail.com>
2021-06-02 17:51:53 -0300

    Script Feedbacks

---
+++ Fukushima (nome super normal)2/Assets/_Fukushima/Scripts/FeedbackHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject feedbackLocal;
+    [SerializeField] private GameObject feedbackPista;
+    [SerializeField] private GameObject feedbackPublicarMateria;
+
+    public enum feedbackType
+    {
+        NovoLocal,
+        NovaPista,
+        PublicarMateria,
+    }
+
+    private void Awake()
+    {
+        feedbackPista.SetActive(false);
+        feedbackLocal.SetActive(false);
+        feedbackPublicarMateria.SetActive(false);
+    }
+
+    public void Feedback(feedbackType type)
+    {
+        if(type == feedbackType.NovaPista) feedbackPista.SetActive(true);
+        else if(type == feedbackType.NovoLocal) feedbackLocal.SetActive(true);
+        else if(type == feedbackType.PublicarMateria) feedbackPublicarMateria.SetActive(true);
+
+        DesativarFeedbacks(); 
+    }
+
+    IEnumerator DesativarFeedbacks() 
+    {
+        yield return new WaitForSeconds(3);
+        feedbackPista.SetActive(false);
+        feedbackLocal.SetActive(false);
+        feedbackPublicarMateria.SetActive(false);
+    }
+}
